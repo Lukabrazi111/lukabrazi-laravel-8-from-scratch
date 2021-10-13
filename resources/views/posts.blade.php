9bf78529a42eb3914ae938f763a8a63c901a7699
lukakhangoshvili <lukakhangoshvili@gmail.com>
2021-10-13 23:15:49 +0400

    make route and link to it

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/app.css">
+    <title>Document</title>
+</head>
+<body>
+    <article>
+        <h1><a href="/post"> My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe eius sapiente rem molestiae aut itaque expedita optio, pariatur odio illo, officiis porro ipsa repellendus tempore voluptatem distinctio impedit alias consectetur vel, autem qui commodi minus laboriosam. Nulla id beatae ex earum saepe quo impedit harum error iure incidunt ratione quis aliquid voluptatem debitis voluptas, nostrum ullam? Dolor perspiciatis ratione ea porro, illum, laborum facilis enim ducimus dolorem veritatis iste doloremque dolorum id. Corrupti ducimus, illo dolorum vel itaque eveniet placeat veniam quibusdam exercitationem praesentium. Earum, assumenda eius, exercitationem laborum quia soluta atque ullam sequi voluptatem ex et repellendus debitis praesentium.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe eius sapiente rem molestiae aut itaque expedita optio, pariatur odio illo, officiis porro ipsa repellendus tempore voluptatem distinctio impedit alias consectetur vel, autem qui commodi minus laboriosam. Nulla id beatae ex earum saepe quo impedit harum error iure incidunt ratione quis aliquid voluptatem debitis voluptas, nostrum ullam? Dolor perspiciatis ratione ea porro, illum, laborum facilis enim ducimus dolorem veritatis iste doloremque dolorum id. Corrupti ducimus, illo dolorum vel itaque eveniet placeat veniam quibusdam exercitationem praesentium. Earum, assumenda eius, exercitationem laborum quia soluta atque ullam sequi voluptatem ex et repellendus debitis praesentium.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe eius sapiente rem molestiae aut itaque expedita optio, pariatur odio illo, officiis porro ipsa repellendus tempore voluptatem distinctio impedit alias consectetur vel, autem qui commodi minus laboriosam. Nulla id beatae ex earum saepe quo impedit harum error iure incidunt ratione quis aliquid voluptatem debitis voluptas, nostrum ullam? Dolor perspiciatis ratione ea porro, illum, laborum facilis enim ducimus dolorem veritatis iste doloremque dolorum id. Corrupti ducimus, illo dolorum vel itaque eveniet placeat veniam quibusdam exercitationem praesentium. Earum, assumenda eius, exercitationem laborum quia soluta atque ullam sequi voluptatem ex et repellendus debitis praesentium.
+        </p>
+    </article>
+</body>
+</html>
