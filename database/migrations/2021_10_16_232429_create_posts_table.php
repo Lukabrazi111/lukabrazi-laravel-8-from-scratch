8462cb504007ba6488d47f56d8f2c16e03878400
lukakhangoshvili <lukakhangoshvili@gmail.com>
2021-10-17 19:27:34 +0400

    feat: added route model binding

--- database/migrations/2021_10_16_232429_create_posts_table.php
+++ database/migrations/2021_10_16_232429_create_posts_table.php
@@ -4,17 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
-{
+class CreatePostsTable extends Migration {
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up()
-    {
+    public function up() {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string('slug')->unique();
             $table->string('title');
             $table->text('excerpt');
             $table->text('body');
@@ -28,8 +27,7 @@ class CreatePostsTable extends Migration
      *
      * @return void
      */
-    public function down()
-    {
+    public function down() {
         Schema::dropIfExists('posts');
     }
 }
