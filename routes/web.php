e0ce9684359444f586350de0086528d510c55780
lukakhangoshvili <lukakhangoshvili@gmail.com>
2021-10-20 21:40:12 +0400

    feat: extract a dropdown blade component

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts', [
         'posts' => Post::latest()->get(),
+        'categories' => Category::all(),
     ]);
-});
+})->name('home');
 
 Route::get('posts/{post:slug}', function (Post $post) {
     return view("post", [
@@ -31,11 +32,14 @@ Route::get('posts/{post:slug}', function (Post $post) {
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'posts' => $category->posts,
+        'currentCategory' => $category,
+        'categories' => Category::all(),
     ]);
-});
+})->name('category');
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
         'posts' => $author->posts,
+        'categories' => Category::all(),
     ]);
 });
