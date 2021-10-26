a006c6f2ddd45a1ddce3ab670a41806b056cd6d7
lukakhangoshvili <lukakhangoshvili@gmail.com>
2021-10-26 06:20:42 +0400

    feat: create failed validation and old input data

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -9,21 +9,37 @@
                     <label for="name" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Name
                     </label>
-                    <input type="text" name="name" id="name" class="border border-gray-400 p-2 w-full" required>
+                    <input
+                        type="text" name="name" id="name" class="border border-gray-400 p-2 w-full" required
+                        value="{{ old('name') }}">
+
+                    @error('name')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
                     <label for="email" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Email
                     </label>
-                    <input type="email" name="email" id="email" class="border border-gray-400 p-2 w-full" required>
+                    <input type="email" name="email" id="email" class="border border-gray-400 p-2 w-full" required
+                           value="{{ old('email') }}">
+
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
                     <label for="username" class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Username
                     </label>
-                    <input type="text" name="username" id="username" class="border border-gray-400 p-2 w-full" required>
+                    <input type="text" name="username" id="username" class="border border-gray-400 p-2 w-full" required
+                           value="{{ old('username') }}">
+
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -32,6 +48,10 @@
                     </label>
                     <input type="password" name="password" id="password" class="border border-gray-400 p-2 w-full"
                            required>
+
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -40,6 +60,14 @@
                     </button>
                 </div>
 
+
+                @if($errors->any())
+                    <ul>
+                        @foreach($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif
             </form>
         </main>
     </section>
