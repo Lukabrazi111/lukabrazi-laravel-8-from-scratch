2863587d4d91a960c94ecf4ab4f3a7469a5c1250
lukakhangoshvili <lukakhangoshvili@gmail.com>
2021-10-26 07:21:53 +0400

    feat: login and logout functionalities

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller {
     public function create() {
@@ -17,7 +18,11 @@ class RegisterController extends Controller {
             'password' => 'required|max:255|min:7',
         ]);
 
-        User::create($attributes);
+        $user = User::create($attributes);
+
+        // Log the user in
+        auth()->login($user); // <-- this = Auth::login($user)
+
 
         session()->flash('success', 'Your account has been created!');
 
