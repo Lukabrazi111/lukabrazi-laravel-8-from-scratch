8bf6df9b1756523f07caf188697c5693bf8c78a1
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2021-10-28 20:01:18 +0400

    feat: install php cs fixer

--- app/Http/Controllers/PostCommentController.php
+++ app/Http/Controllers/PostCommentController.php
@@ -6,17 +6,17 @@ use App\Models\Post;
 
 class PostCommentController extends Controller
 {
-    public function store(Post $post)
-    {
-        request()->validate([
-            'body' => 'required',
-        ]);
+	public function store(Post $post)
+	{
+		request()->validate([
+			'body' => 'required',
+		]);
 
-        $post->comments()->create([
-            'user_id' => request()->user()->id,
-            'body' => request('body'),
-        ]);
+		$post->comments()->create([
+			'user_id' => request()->user()->id,
+			'body'    => request('body'),
+		]);
 
-        return back();
-    }
+		return back();
+	}
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,17 +4,19 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 
-class PostController extends Controller {
-    public function index() {
+class PostController extends Controller
+{
+	public function index()
+	{
+		return view('posts.index', [
+			'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString(),
+		]);
+	}
 
-        return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->paginate(6)->withQueryString(),
-        ]);
-    }
-
-    public function show(Post $post) {
-        return view("posts.show", [
-            "post" => $post,
-        ]);
-    }
+	public function show(Post $post)
+	{
+		return view('posts.show', [
+			'post' => $post,
+		]);
+	}
 }
