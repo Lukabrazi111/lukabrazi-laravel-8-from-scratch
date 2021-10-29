9db4b1ce92a0407e7031772e8b19553c5d1c445a
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2021-10-29 13:16:04 +0400

    feat: extract a newsletter service

--- config/services.php
+++ config/services.php
@@ -30,6 +30,9 @@ return [
 	],
 
 	'mailchimp' => [
-		'key' => env('MAILCHIMP_KEY'),
+		'key'   => env('MAILCHIMP_KEY'),
+		'lists' => [
+			'subscribers' => env('MAILCHIMP_LIST_SUBSCRIBERS'),
+		],
 	],
 ];
