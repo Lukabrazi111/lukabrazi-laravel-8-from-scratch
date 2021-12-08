c1fb6f6332b8f29522336fbff823c05bbe6137c1
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2021-12-08 11:22:01 +0400

    chore: add .env.example file

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder {
             'name' => 'John Doe',
         ]);
 
-        Post::factory(5)->create([
+        Post::factory(6)->create([
             'user_id' => $user->id,
         ]);
     }
