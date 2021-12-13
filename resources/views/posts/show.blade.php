1ea7354cc6e55545baf9b26d42ef642a5b5b43eb
Luka Khangoshvili <lukakhangoshvili@gmail.com>
2021-12-13 08:04:33 +0400

    chore: use routes in blade components

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,7 +14,7 @@
                         <img src="{{ asset('images/lary-avatar.svg') }}" alt="Lary avatar">
                         <div class="ml-3 text-left">
                             <h5 class="font-bold">
-                                <a href="/?authors={{ $post->author->username }}">{{ $post->author->name }}</a>
+                                <a href="{{ route('home', ['authors' => $post->author->username]) }}">{{ $post->author->name }}</a>
                             </h5>
                         </div>
                     </div>
